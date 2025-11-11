bbc1804c399b4d3a86e6f77ceb97b49499936cda
RMrobbie <robbiemariano126@gmail.com>
2025-11-11 20:35:55 +0800

    feat: add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/next";
+
 import Index from "./pages/index";
 import NotFound from "./pages/NotFound";
 
@@ -21,6 +23,7 @@ const App = () => (
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
+    <Analytics />
   </QueryClientProvider>
 );
 
